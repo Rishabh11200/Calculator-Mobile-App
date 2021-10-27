a10ceed8e016b39a263c42d287c40ae75d1dc568
Rishabh11200 <rishabhshah1100@gmail.com>
2021-10-27 16:15:34 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+
+import React from 'react';  
+import CalcScreen from './CalcScreen';
+
+export default class App extends React.Component {  
+  render() {  
+    return <CalcScreen/>;  
+  }  
+}  
